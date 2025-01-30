bab1cf1e7c8828601d949cec3d23c634fa1a0bd0
Ana Ward <amandagward@me.com>
2025-01-29 21:54:18 -0500

    added photo id route

--- routes/photos.js
+++ routes/photos.js
@@ -7,5 +7,18 @@ router.get("/", function (request, response) {
     response.json(photos);
 });
 
+router.get("/:id", function (request, response) {
+    const photo = photos.find((e) => e.id === request.params.id);
+    // looking through the array of photos and finding the one object that matches the parameter
+    if (photo === undefined) {
+        response.status(404).send("Photo not found")
+        // returns error code
+    }
+    else {
+        response.json(photo)
+    }
+    // returns photo via json
+})
+
 export default router;
 // imported to index.js as photoRoutes
