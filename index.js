aa96ef2cbfcfd2c04e517c9717ed7475f6bc41aa
Ana Ward <amandagward@me.com>
2025-01-29 21:50:13 -0500

    added tag routes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import tagRoutes from './routes/tags.js';
 
 const app = express();
+
+app.use('/tags', tagRoutes);
 
 app.listen(8080, () => {
     console.log('Server Started on http://localhost:8080');
