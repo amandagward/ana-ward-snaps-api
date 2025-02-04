4565d31531f17f3c21dee183827fc50a65ea3f50
Ana Ward <amandagward@me.com>
2025-02-03 21:02:49 -0500

    added .env and cors and adjsuted images

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import tagRoutes from './routes/tags.js';
 import photoRoutes from './routes/photos.js';
+import cors from 'cors';
 
 const app = express();
-
+app.use(cors())
 app.use(express.static("public"))
 app.use(express.json())
 app.use('/tags', tagRoutes);
 app.use('/photos', photoRoutes);
 
-app.listen(8080, () => {
+app.listen(process.env.PORT, () => {
     console.log('Server Started on http://localhost:8080');
     console.log('Press CTRL + C to stop server');
 });
